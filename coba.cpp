57fafa8c06ffbccc4c2b56b7117c805ba57ddfc1
andikaramaa <andikaaprianto86@gmail.com>
2023-11-11 15:42:07 +0700

    RobotikARMBROOOOOOOO

--- coba.cpp
+++ coba.cpp
@@ -1,6 +1,11 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
+// Define servo parameters
+#define NUM_SERVOS 4
+#define SERVOMIN 150
+#define SERVOMAX 600
+
 #define SERVO_DRIVER_ADDRESS 0x40  // Alamat I2C default Servo Driver PCA9685
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(SERVO_DRIVER_ADDRESS);
 
@@ -13,13 +18,23 @@ void setup() {
 }
 
 void loop() {
-  moveServoSlowly(0, 90, 180, 5);  // Gerakkan servo dari posisi 0 ke 180 derajat dengan langkah 5 derajat
+  
+  moveServoSlowly(0, 0, 180, 5);  // Gerakkan servo dari posisi 0 ke 180 derajat dengan langkah 5 derajat
+  moveServoSlowlymin(0, 180, 0, 5);
 }
 
 void moveServoSlowly(uint8_t servoNum, uint16_t fromPos, uint16_t toPos, uint8_t stepSize) {
   // Gerakkan servo dari posisi awal ke posisi akhir dengan langkah tertentu
   for (uint16_t pos = fromPos; pos <= toPos; pos += stepSize) {
     pwm.setPWM(servoNum, 0, map(pos, 0, 180, SERVOMIN, SERVOMAX));
-    delay(50);  // Sesuaikan nilai delay sesuai kebutuhan untuk memperlambat gerakan
+    delay(400);  // Sesuaikan nilai delay sesuai kebutuhan untuk memperlambat gerakan
+  }
+}
+
+void moveServoSlowlymin(uint8_t servoNum, uint16_t fromPos, uint16_t toPos, uint8_t stepSize) {
+  // Gerakkan servo dari posisi awal ke posisi akhir dengan langkah tertentu
+  for (uint16_t pos = fromPos; pos >= toPos; pos -= stepSize) {
+    pwm.setPWM(servoNum, 0, map(pos, 0, 180, SERVOMIN, SERVOMAX));
+    delay(400);  // Sesuaikan nilai delay sesuai kebutuhan untuk memperlambat gerakan
   }
 }
