50c5f2c1e84be52058c32d99038e6dcceb860905
Lyalmoon <rasyidn0812@gmail.com>
2023-11-11 10:27:10 +0700

    CheckRobotic

---
+++ checkroboticarm.cpp
@@ -0,0 +1,42 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm;
+
+// Define servo parameters
+#define NUM_SERVOS 4
+#define SERVO_MIN 150
+#define SERVO_MAX 600
+
+void setup() {
+  Serial.begin(9600);
+  pwm.begin();
+  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+  delay(10);
+}
+
+void loop() {
+  for (int servoNum = 0; servoNum < NUM_SERVOS; servoNum++) {
+    // Move each servo to the minimum position
+    Serial.print("Moving Servo ");
+    Serial.print(servoNum + 1);
+    Serial.println(" to minimum position");
+    pwm.setPWM(servoNum, 0, SERVO_MIN);
+    delay(1000);  // Wait for the servo to reach the minimum position
+
+    // Move each servo to the maximum position
+    Serial.print("Moving Servo ");
+    Serial.print(servoNum + 1);
+    Serial.println(" to maximum position");
+    pwm.setPWM(servoNum, 0, SERVO_MAX);
+    delay(1000);  // Wait for the servo to reach the maximum position
+
+    // Return each servo to the center position
+    Serial.print("Returning Servo ");
+    Serial.print(servoNum + 1);
+    Serial.println(" to center position");
+    int centerPosition = (SERVO_MIN + SERVO_MAX) / 2;
+    pwm.setPWM(servoNum, 0, centerPosition);
+    delay(1000);  // Wait for the servo to return to the center position
+  }
+}
