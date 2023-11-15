a4d084a56b3193f1d2a418576144b919858efc00
oddep <adityasanjaya787@gmail.com>
2023-11-15 12:29:08 +0700

    Try implementing movement robotic arm

--- sismik.cpp
+++ sismik.cpp
@@ -1,5 +1,13 @@
 #include <Servo.h>
 #include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+#define NUM_SERVOS 4
+#define SERVOMIN 150
+#define SERVOMAX 600
+#define SERVO_DRIVER_ADDRESS 0x40  // Alamat I2C default Servo Driver PCA9685
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(SERVO_DRIVER_ADDRESS);
+
 
 //DEKLARASI I2C
 int alamatSlave = 4;
@@ -72,6 +80,29 @@ class TCS3200 {
 };
 
 //MAIN
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Adafruit PWM Servo Driver Test");
+
+  pwm.begin();
+  pwm.setPWMFreq(60);  // Set frekuensi PWM ke 60 Hz (sesuaikan dengan servo)
+}
+
+void moveServoSlowly(uint8_t servoNum, uint16_t fromPos, uint16_t toPos, uint8_t stepSize, unsigned long time) {
+  // Gerakkan servo dari posisi awal ke posisi akhir dengan langkah tertentu
+  for (uint16_t pos = fromPos; pos <= toPos; pos += stepSize) {
+    pwm.setPWM(servoNum, 0, map(pos, 0, 180, SERVOMIN, SERVOMAX));
+    delay(time);  // Sesuaikan nilai delay sesuai kebutuhan untuk memperlambat gerakan
+  }
+}
+
+void moveServoSlowlymin(uint8_t servoNum, uint16_t fromPos, uint16_t toPos, uint8_t stepSize, unsigned long time) {
+  // Gerakkan servo dari posisi awal ke posisi akhir dengan langkah tertentu
+  for (uint16_t pos = fromPos; pos >= toPos; pos -= stepSize) {
+    pwm.setPWM(servoNum, 0, map(pos, 0, 180, SERVOMIN, SERVOMAX));
+    delay(time);  // Sesuaikan nilai delay sesuai kebutuhan untuk memperlambat gerakan
+  }
+}
  
 
 void loop() {
@@ -119,6 +150,8 @@ void loop() {
                Serial.println("Terdeteksi");
                //MEMATIKAN RELAY
                digitalWrite(relay, HIGH);
+               moveServoSlowly(0, 0, 180, 5, 400);  // Gerakkan servo dari posisi 0 ke 180 derajat dengan langkah 5 derajat
+               moveServoSlowlymin(0, 180, 0, 5, 400);
           }
      }
      delay(2000);
